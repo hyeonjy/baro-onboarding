94ada4cd401338327b1dcb8be93ba7ee8f971d51
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-03-15 19:26:59 +0900

    feat: Todo 리스트 prefetch 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
 import TodoInput from "@/components/TodoInput";
 import TodoList from "@/components/TodoList";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { fetchTodos } from "@/lib/todoApi";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const Home = () => {
+const Home = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
   return (
     <div className="w-full max-w-2xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-8 text-center">Todo List</h1>
-      <ErrorBoundary>
-        <TodoInput />
-      </ErrorBoundary>
 
-      <ErrorBoundary>
-        <TodoList />
-      </ErrorBoundary>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ErrorBoundary>
+          <TodoInput />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <TodoList />
+        </ErrorBoundary>
+      </HydrationBoundary>
     </div>
   );
 };
